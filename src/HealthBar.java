53d5412e77949f71c1b0f3485cc9b4a2afa99d50
CRACKz-cyber <lasse.wiedemann@icloud.com>
2024-06-21 15:22:05 +0200

    Judith
    20.06

---
+++ src/HealthBar.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class HealthBar
+extends Traveler
+
+{
+    private int leben;
+    private int maxLeben;
+    private Image healthBar;
+    private Traveler Traveler;
+
+    public HealthBar(int maxLeben, String imagePath) {
+        this.maxLeben = maxLeben;
+        this.leben = maxLeben;
+        this.healthBar = new ImageIcon("gegnerHealth3.png").getImage();
+        this.Traveler = Traveler;
+        
+    }
+
+    public void draw(Graphics g) {
+        for (int i = 0; i < leben; i++) {
+            g.drawImage(healthBar, 20 + i * 35, 20, null);
+        }
+    }
+
+    public void loseLife() {
+        if (leben > 0) {
+            leben--;
+        }
+    }
+
+    public int getLives() {
+        return leben;
+    }
+}
+
+      
